9cd589958e5159e0b85c1c09839f6284479e8dbf
Jesse Kane <jessekane66@gmail.com>
2025-06-02 13:01:28 -0400

    began work on svcore

---
+++ svcore/src/native/cpp/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "visiontypes.h"
+
+int main() {
+    std::cout << "Placeholder at the moment, this will be the main entry point into SLAMVision" << std::endl;
+    return 0;
+}
