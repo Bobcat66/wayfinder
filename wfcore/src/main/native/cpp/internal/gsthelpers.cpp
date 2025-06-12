02c8c0d1618e68232158d0738047b3781e022514
Jesse Kane <jessekane66@gmail.com>
2025-06-12 04:23:42 -0400

    e

--- wfcore/src/main/native/cpp/internal/gsthelpers.cpp
+++ wfcore/src/main/native/cpp/internal/gsthelpers.cpp
@@ -29,7 +29,10 @@ namespace wf::impl {
             GstPlugin *plugin = (GstPlugin *)l->data;
             const gchar *license = gst_plugin_get_license(plugin);
             const gchar *name = gst_plugin_get_name(plugin);
-            if (licenseAllowlist.find(license) == licenseAllowlist.end()) {pluginBlocklist.emplace(name);}
+            if (licenseAllowlist.find(license) == licenseAllowlist.end()) {
+                logger->debug("Blocked plugin {}",name);
+                pluginBlocklist.emplace(name);
+            }
             gst_object_unref(plugin);
         }
 
@@ -43,7 +46,7 @@ namespace wf::impl {
     }
 
     bool searchForGSTPlugin(const std::string& pluginName) {
-        logger->trace("searching for plugin %s",pluginName);
+        logger->trace("searching for plugin {}",pluginName);
         auto map_it = queriedPlugins.find(pluginName);
 
         if (map_it != queriedPlugins.end()) {
@@ -51,7 +54,7 @@ namespace wf::impl {
         }
 
         if (GSTPluginBlocked(pluginName)) {
-            logger->warn("Plugin %s is blocked due to potential license incompatibility",pluginName);
+            logger->warn("Plugin {} is blocked due to potential license incompatibility",pluginName);
             queriedPlugins[pluginName] = false;
             return false;
         }
@@ -70,19 +73,19 @@ namespace wf::impl {
         return found;
     }
 
-    bool loadGSTPlugin(const std::string pluginName) {
+    bool loadGSTPlugin(const std::string& pluginName) {
 
         if (loadedPlugins.find(pluginName) != loadedPlugins.end()) {
-            logger->debug("Plugin %s already loaded",pluginName);
+            logger->debug("Plugin {} already loaded",pluginName);
             return true;
         }
 
         if (!searchForGSTPlugin(pluginName)){
-            logger->warn("Plugin %s not found in registry",pluginName);
+            logger->warn("Plugin {} not found in registry",pluginName);
             return false;
         }
 
-        logger->info("Attempting to load plugin %s",pluginName);
+        logger->info("Attempting to load plugin {}",pluginName);
         GstPlugin* plugin = gst_plugin_load_by_name(pluginName.c_str());
 
         if (plugin) {
@@ -91,34 +94,46 @@ namespace wf::impl {
             return true;
         }
 
-        logger->error("Plugin %s not found",pluginName);
+        logger->error("Plugin {} not found",pluginName);
         return false;
     }
 
     GstElement* getGSTElement(const std::string& elementType, const std::string& instanceName) {
         GstRegistry* registry = gst_registry_get();
-        GstPluginFeature* factory = gst_registry_lookup_feature(registry, elementType.c_str());
-        
-        if (!factory) {
-            logger->warn("No factory found for element type %s", elementType);
+        GstPluginFeature* feature = gst_registry_lookup_feature(registry, elementType.c_str());
+
+        if (!feature) {
+            logger->warn("No factory found for element type {}", elementType);
             return nullptr;
         }
 
-        const gchar* pluginName = gst_plugin_feature_get_plugin_name(factory);
-        logger->debug("Element type %s part of plugin %s",elementType,pluginName);
+        const gchar* pluginName = gst_plugin_feature_get_plugin_name(feature);
+        logger->debug("Element type {} part of plugin {}",elementType,pluginName);
 
         if (GSTPluginBlocked(pluginName)) {
-            gst_object_unref(factory);
+            gst_object_unref(feature);
             return nullptr;
         }
 
         if (!loadGSTPlugin(pluginName)) {
-            logger->warn("Failed to load plugin %s",pluginName);
-            gst_object_unref(factory);
+            logger->warn("Failed to load plugin {}",pluginName);
+            gst_object_unref(feature);
             return nullptr;
         }
 
-        auto plugin = 
+        if (GST_IS_ELEMENT_FACTORY(feature)) {
+            GstElementFactory* factory = GST_ELEMENT_FACTORY(feature);
+            GstElement* element = gst_element_factory_create(
+                factory,
+                instanceName.c_str()
+            );
+            gst_object_unref(feature);
+            return element;
+        } else {
+            logger->debug("Feature is not a factory");
+            gst_object_unref(feature);
+            return nullptr;
+        }
     }
 
 }
