d22112dd411c3631ea922591b9e1784019ca5ad2
Jesse Kane <jessekane66@gmail.com>
2025-08-18 20:35:17 -0400

    sjs

--- daemon/src/main/native/main.cpp
+++ daemon/src/main/native/main.cpp
@@ -42,6 +42,8 @@ int main() {
     std::cout << "Starting Wayfinder v" << PROJECT_VERSION << std::endl;
     try {
         auto wayfinder = wf::WFOrchestrator::createFromEnv();
+        wayfinder.configureHardware();
+        wayfinder.configureWorkers();
         while (true) {
             wayfinder.periodic();
         }
